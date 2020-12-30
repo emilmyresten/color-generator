775db49b2f4d38bf40c0499013725447a5ec61ce
Emil Myresten <emilmyresten@Emils-MacBook-Pro-2.local>
2020-12-30 17:00:49 +0100

    monochrome functional

--- src/colorGenerator.js
+++ src/colorGenerator.js
@@ -3,36 +3,35 @@ import React from 'react';
 
 const ColorGenerator = () => {
     //max är 16777215, min är 0.
-    const [primaryColor, setPrimaryColor] = React.useState(new Array(5).fill("0").map(() => getRandomInt(16777215).toString(16)));
-    const [fontColor, setFontColor] = React.useState(new Array(5).fill("000000"));
+    // const [primaryColor, setPrimaryColor] = React.useState(new Array(5).fill("0").map(() => getRandomInt(16777215).toString(16)));
+    const [primaryColor, setPrimaryColor] = React.useState(padHexCode(getRandomInt(16777215).toString(16)));
+    const [fontColor, setFontColor] = React.useState("000000");
+    const [monoChrome, setMonoChrome] = React.useState(generateMonochrome(primaryColor))
 
+    console.log(monoChrome);
     React.useEffect(()=> {
-        for (let i = 0; i < primaryColor.length; i++) {
-            if (primaryColor[i].length !== 6) {
-                primaryColor[i] = new Array(6-primaryColor.length).fill("0").join("")+primaryColor[i];
-            }
-            getLuma(primaryColor[i]) > 0.5 ? fontColor[i] = "000000" : fontColor[i] = "FFFFFF";
-        }
+        setMonoChrome(generateMonochrome(primaryColor))
+        getLuma(primaryColor) > 0.5 ? setFontColor("000000") : setFontColor("FFFFFF");
     }, [primaryColor])
 
     const handleSpace = () => {
-        setPrimaryColor(new Array(5).fill("0").map(() => getRandomInt(16777215).toString(16)));
+        setPrimaryColor(padHexCode(getRandomInt(16777215).toString(16)));
     }
 
 
 
     const styles=[
-        {backgroundColor: `#${primaryColor[0]}`, color: `#${fontColor[0]}`},
-        {backgroundColor: `#${primaryColor[1]}`, color: `#${fontColor[1]}`},
-        {backgroundColor: `#${primaryColor[2]}`, color: `#${fontColor[2]}`},
-        {backgroundColor: `#${primaryColor[3]}`, color: `#${fontColor[3]}`},
-        {backgroundColor: `#${primaryColor[4]}`, color: `#${fontColor[4]}`}
+        {backgroundColor: `#${monoChrome[0]}`, color: `#${fontColor}`},
+        {backgroundColor: `#${monoChrome[1]}`, color: `#${fontColor}`},
+        {backgroundColor: `#${monoChrome[2]}`, color: `#${fontColor}`},
+        {backgroundColor: `#${monoChrome[3]}`, color: `#${fontColor}`},
+        {backgroundColor: `#${monoChrome[4]}`, color: `#${fontColor}`}
     ]
 
     return (
         <div
         onKeyDown={(e)=> e.key===" " ? handleSpace() : undefined}>
-            <ColorView styles={styles} handleSpace={handleSpace}/>
+            <ColorView styles={styles}/>
         </div>
     )
 }
@@ -47,9 +46,95 @@ function getRandomInt(max) {
   }
 
 
-function getLuma(hexCode) {
+function getRgb(hexCode) {
+    //receives hexcode without #, ex: 374c2c
+    //returns rgb dict between 0-255
     const parsed = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexCode);
     const rgb = {r: parseInt(parsed[1], 16), g: parseInt(parsed[2], 16), b: parseInt(parsed[3], 16)};
+    return rgb
+}
+
+function getHex(rgb) {
+    //receives rgb as dict, ex: {r: 133, g: 13, b: 88}
+    const r = rgb.r.toString(16).length == 2 ? rgb.r.toString(16) : `0${rgb.r.toString(16)}`
+    const g = rgb.g.toString(16).length == 2 ? rgb.g.toString(16) : `0${rgb.g.toString(16)}`
+    const b = rgb.b.toString(16).length == 2 ? rgb.b.toString(16) : `0${rgb.b.toString(16)}`
+    const hexCode = `${r}${g}${b}`
+    if (hexCode.length != 6) {
+        return padHexCode(hexCode)
+    }
+    return hexCode
+}
+
+
+function rgbToHsl(rgb) {
+    // receives rgb as dict, ex: {r: 133, g: 13, b: 88}
+    // returns hsl as dict, {h: 216, s: 0.2, l: 0.5}
+    const normalized = [rgb.r/255, rgb.g/255, rgb.b/255];
+    const max =  Math.max(...normalized);
+    const min = Math.min(...normalized);
+
+    const hueCalculations = [
+        (normalized[1]-normalized[2])/(max-min), 
+        2 + (normalized[2]-normalized[0])/(max-min),
+        4 + (normalized[0]-normalized[1])/(max-min)
+    ]
+    
+    const luminance = (max+min) / 2;
+    const saturation = max === min ? 0 : luminance <= 0.5 ? (max-min)/(max+min) :  (max-min)/(2-max-min)
+    const hue = saturation === 0 ? 0 : hueCalculations[normalized.indexOf(max)] < 0 ? ((hueCalculations[normalized.indexOf(max)]*60) + 360): hueCalculations[normalized.indexOf(max)] * 60
+
+
+    return {
+        h: hue,
+        s: saturation,
+        l: luminance
+    }
+}
+
+function hslToRgb(hsl) {
+    if (hsl.s === 0) { // if we have no saturation we have a shade of grey, i.e. all rgb are the same.
+        return {r: hsl.l*255, g: hsl.l*255, b: hsl.l*255}
+    }
+    const t_1 = hsl.l < 0.5 ? hsl.l * (1+hsl.s) : (hsl.l + hsl.s) - (hsl.l * hsl.s);
+    const t_2 = 2 * hsl.l - t_1
+    
+    const hue = hsl.h / 360;
+
+    const t_r = hue + 0.333 < 0 ? hue + 0.333 + 1 : hue + 0.333 > 1 ? hue + 0.333 - 1 : hue + 0.333;
+    const t_g = hue;
+    const t_b = hue - 0.333 < 0 ? hue - 0.333 + 1 : hue - 0.333 > 1 ? hue - 0.333 - 1 : hue - 0.333;
+
+    return {
+        r: Math.round((6 * t_r < 1 ? t_2 + (t_1 - t_2) * 6 * t_r : 2 * t_r < 1 ? t_1 : 3 * t_r < 2 ? t_2 + (t_1 - t_2) * (0.666-t_r) * 6 : t_2) * 255),
+        g: Math.round((6 * t_g < 1 ? t_2 + (t_1 - t_2) * 6 * t_g : 2 * t_g < 1 ? t_1 : 3 * t_g < 2 ? t_2 + (t_1 - t_2) * (0.666-t_g) * 6 : t_2) * 255),
+        b: Math.round((6 * t_b < 1 ? t_2 + (t_1 - t_2) * 6 * t_b : 2 * t_b < 1 ? t_1 : 3 * t_b < 2 ? t_2 + (t_1 - t_2) * (0.666-t_b) * 6 : t_2) * 255),
+    }
+}
+
+
+function getLuma(hexCode) {
+    const rgb = getRgb(hexCode);
     const luma = (0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b) / 255;
     return luma;
+}
+
+function padHexCode(hexCode) {
+    return new Array(6-hexCode.length).fill("0").join("")+hexCode
+}
+
+function generateMonochrome(primaryColor) {
+    //want to return an array of lenght 5, with each element being rgb
+    // objects based on the primary color with decreasing luminosity.
+    const rgb = getRgb(primaryColor);
+    const hsl = rgbToHsl(rgb);
+    const weigth = 10;
+    const arr = new Array(5);
+
+    for (let i = 0; i < arr.length; i++) {
+        const color = {...hsl} // create a copy of hsl in order to not mutate it.
+        color.l /= (i+1)
+        arr[i] = color
+    }
+    return arr.map(hsl => hslToRgb(hsl)).map(rgb => getHex(rgb));
 }
